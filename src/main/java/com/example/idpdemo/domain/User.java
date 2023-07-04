e7882f450b625053e3ee955b114c8d8152204e1f
hyejun8701 <hyejun8701@gmail.com>
2023-07-05 00:26:14 +0900

    init

---
+++ src/main/java/com/example/idpdemo/domain/User.java
@@ -0,0 +1,22 @@
+package com.example.idpdemo.domain;
+
+public class User {
+    private String loginId;
+    private String email;
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
